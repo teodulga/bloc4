bc72bad3a388bb9ccae8bd70178a189626511ead
Téodul <115618828+teodulga@users.noreply.github.com>
2025-03-20 13:35:12 +0000

    aa

--- eval/eval.py
+++ eval/eval.py
@@ -1,39 +1,115 @@
 import algosdk as sdk
 import algokit_utils as au
 import client as cl
+import algokit_utils.transactions.transaction_composer as att
 
-algorand = au.AlgorandClient.from_environment()
+# def account_creation(algorand: au.AlgorandClient, name: str, funds=au.AlgoAmount(algo=0)):
+#     account = algorand.account.from_environment(name, fund_with=funds)
+#     info = algorand.client.algod.account_info(account.address)
+#     print(
+#         f"Name\t\t: %s \n"
+#         f"Address\t\t: %s\n"
+#         f"Created Asset\t: %s\n"
+#         f"Assets\t\t: %s\n"
+#         f"Algo\t\t: %.6f"
+#         % (
+#             name,
+#             account.address,
+#             info["created-assets"],
+#             info["assets"],
+#             info["amount"] / 1_000_000,
+#         )
+#     )
+#     if len(info["created-apps"]) > 0:
+#         print(f"Created-Apps\t: %s \n" % info["created-apps"][0]["id"])
+#     print("")
+#     return account
+
+algorand = au.AlgorandClient.testnet()
 
 code = "pioneer road rug owner lava slogan whale atom ramp motion oak execute snow mirror sight squirrel toward mention adjust become grape ridge basic abstract able"
 
 account = algorand.account.from_mnemonic(mnemonic=code)
-public_address = account.address
 
-account_manager = au.AccountManager(algorand.client)
-algorand.account.ensure_funded(account, account_manager.dispenser_from_environment(), min_spending_balance=1_000_000)
+# alice = account_creation(algorand, "ALICE", au.AlgoAmount(algo=10000))
 
-try:
-    factory = algorand.client.get_typed_app_factory(
-        cl.EvalFactory, default_sender=public_address
+eval_factory = cl.EvalFactory(
+    algorand=algorand,
+    default_sender=account.address,
+)
+
+app_id = 736038676
+
+
+ac = eval_factory.get_app_client_by_id(app_id, default_sender=account.address, default_signer=account.signer)
+
+# algorand.account.ensure_funded(account, alice, min_spending_balance=au.AlgoAmount(micro_algo=1_000_000), min_funding_increment=au.AlgoAmount(micro_algo=1_000_000))
+# algorand.account.ensure_funded(ac.app_address, alice, min_spending_balance=au.AlgoAmount(micro_algo=1_000_000), min_funding_increment=au.AlgoAmount(micro_algo=1_000_000))
+
+box_key = account.public_key
+
+# args = cl.AddStudentsArgs(account=account.address)
+
+# param = au.CommonAppCallParams(box_references=[box_key])
+
+# ac.send.add_students(args, param)
+
+# q1 = b"q1" + box_key
+
+# param = au.CommonAppCallParams(box_references=[box_key, q1])
+
+# ac.send.claim_algo(param)
+
+result = algorand.send.asset_create(
+        au.AssetCreateParams(
+            sender=account.address,
+            signer=account.signer,
+            total=15,
+            decimals=0,
+            default_frozen=False,
+            unit_name="PY-CL-FD",
+            asset_name="Proof of Attendance Py-Clermont",
+            url="https://pyclermont.org/",
+            note="Hello Clermont",
+        )
+    )
+asset_id = result.confirmation["asset-index"]
+
+mbr_pay_txn = algorand.create_transaction.payment(
+        au.PaymentParams(
+            sender=account.address,
+            receiver=ac.app_address,
+            amount=au.AlgoAmount(algo=0.2),
+            extra_fee=au.AlgoAmount(micro_algo=1_000),
+        )
     )
 
-    result, _ = factory.send.create.bare()
-    app_id = result.app_id
+args = cl.OptInToAssetArgs(
+    mbr_pay=att.TransactionWithSigner(mbr_pay_txn, account.signer),
+    asset=asset_id,
+)
 
-    ac = factory.get_app_client_by_id(app_id, default_sender=public_address)
-except sdk.error.AlgodHTTPError as e:
-    if e.code == 400 and e.message == "Application already exists":
-        app_id = 1
-        ac = factory.get_app_client_by_id(app_id, default_sender=public_address)
-    else:
-        raise
+q2 = b"q2" + box_key
 
-box_key = public_address
+param = au.CommonAppCallParams(box_references=[box_key, q2])
 
-args = cl.AddStudentsArgs(account=public_address)
+ac.send.opt_in_to_asset(args, param)
 
-param = au.CommonAppCallParams(box_references=[box_key])
+q3 = b"q3" + box_key
 
-ac.send.claim_algo(param)
+array = [1, 2]
+byte_array = bytes(array)
 
-ac.send.add_students(args, param)
+args = cl.SumArgs(array=byte_array)
+
+param = au.CommonAppCallParams(box_references=[box_key, q3])
+
+ac.send.sum(args, param)
+
+q4 = b"q4" + box_key
+
+args = cl.UpdateBoxArgs(value="Hello")
+
+param = au.CommonAppCallParams(box_references=[box_key, q4])
+
+ac.send.update_box(args, param)
